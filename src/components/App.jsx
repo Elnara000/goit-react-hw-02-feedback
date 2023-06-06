76550e7330388005b95f547ccf669538ddc92634
Elnara000 <110972810+Elnara000@users.noreply.github.com>
2023-06-06 17:43:56 +0300

    feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = e => {
+    const btnName = e.currentTarget.textContent.toLowerCase();
+    if (btnName === 'good') {
+      this.setState(prevState => ({
+        good: prevState.good + 1,
+      }));
+    } else if (btnName === 'neutral') {
+      this.setState(prevState => ({
+        neutral: prevState.neutral + 1,
+      }));
+    } else if (btnName === 'bad') {
+      this.setState(prevState => ({
+        bad: prevState.bad + 1,
+      }));
+    }
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const goodFeedback = this.state.good;
+    if (goodFeedback) {
+      return Math.round((goodFeedback / this.countTotalFeedback()) * 100);
+    } else return 0;
+  };
+
+  render() {
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions onLeaveFeedback={this.onLeaveFeedback} />
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,15 @@
+const FeedbackOptions = ({ onLeaveFeedback }) => (
+  <>
+    <button type="button" onClick={onLeaveFeedback}>
+      Good
+    </button>
+    <button type="button" onClick={onLeaveFeedback}>
+      Neutral
+    </button>
+    <button type="button" onClick={onLeaveFeedback}>
+      Bad
+    </button>
+  </>
+);
+
+export default FeedbackOptions;
